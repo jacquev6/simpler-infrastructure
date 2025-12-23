dc10063e0f235869e2f83d1f92ccc73dcc66490a
Vincent Jacques <vincent@vincent-jacques.net>
2025-12-23 10:09:23 +0100

    Remove Patty and Gabby

--- apply.sh
+++ apply.sh
@@ -7,7 +7,7 @@ set -o nounset
 doorman=192.168.1.101  # Fixed address given by permanent DHCP lease configured on Freebox router
 # Port forwarding for ports 80 and 443 to 'doorman' is configured on the Freebox router
 
-ssh $doorman mkdir -p ~/web-server/letsencrypt ~/web-server/patty/classification-models ~/web-server/patty/images-detection-models
+ssh $doorman mkdir -p ~/web-server/letsencrypt
 
 rsync \
   --verbose \
@@ -20,7 +20,3 @@ rsync \
 
 # Add '--force-recreate' when added a new site so that NGinx-Certbot generates new certificates on startup
 ssh $doorman "cd web-server/compose; docker compose up --remove-orphans --detach"
-echo "Alembic upgrade"
-ssh $doorman "cd web-server/compose; docker compose exec --workdir /app/patty patty-backend alembic upgrade head"
-echo "Data migration"
-ssh $doorman "cd web-server/compose; docker compose exec patty-backend python -m patty migrate-data"
